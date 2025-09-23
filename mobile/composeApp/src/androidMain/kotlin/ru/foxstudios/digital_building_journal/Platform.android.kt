7b0bc7de241993dfeb5b766d622bcdea98d90889
agniaendie <endienasg2@gmail.com>
2025-09-23 09:26:25 +0500

    setup mobile project without libs

---
+++ mobile/composeApp/src/androidMain/kotlin/ru/foxstudios/digital_building_journal/Platform.android.kt
@@ -0,0 +1,9 @@
+package ru.foxstudios.digital_building_journal
+
+import android.os.Build
+
+class AndroidPlatform : Platform {
+    override val name: String = "Android ${Build.VERSION.SDK_INT}"
+}
+
+actual fun getPlatform(): Platform = AndroidPlatform()
---
+++ mobile/composeApp/src/iosMain/kotlin/ru/foxstudios/digital_building_journal/Platform.ios.kt
@@ -0,0 +1,9 @@
+package ru.foxstudios.digital_building_journal
+
+import platform.UIKit.UIDevice
+
+class IOSPlatform: Platform {
+    override val name: String = UIDevice.currentDevice.systemName() + " " + UIDevice.currentDevice.systemVersion
+}
+
+actual fun getPlatform(): Platform = IOSPlatform()
