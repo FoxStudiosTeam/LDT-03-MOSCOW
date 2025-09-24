be71818d0776816f7b1153cc0bf074cb432b964a
LAKKERT <lakkert2002@yandex.ru>
2025-09-24 22:31:54 +0500

    - Edit/delete/create steps.

--- frontend/src/app/list_objects/create_object/second_step/page.tsx
+++ frontend/src/app/list_objects/create_object/second_step/page.tsx
@@ -1,8 +1,15 @@
 'use client';
 
 import { Header } from "@/app/components/header";
+import { useActionsStore } from "@/storage/jobsStorage";
+import Link from "next/link";
+import Image from "next/image";
 
 export default function SecondStep() {
+
+    const jobsData = useActionsStore((state) => state.jobs);
+    const deleteJob = useActionsStore((state) => state.deleteJob)
+
     return (
         <div className="flex justify-center bg-[#D0D0D0] mt-[50px]">
             <Header />
@@ -31,11 +38,25 @@ export default function SecondStep() {
                                 <th className="w-[80px]"></th>
                             </tr>
                         </thead>
+                        <tbody>
+                            {jobsData.map((item, itemIdx) => (
+                                <tr key={itemIdx}>
+                                    <td>{itemIdx + 1}</td>
+                                    <td>{item.title}</td>
+                                    <td>{item.startDate}</td>
+                                    <td>{item.endDate}</td>
+                                    <td className="flex flex-row gap-3">
+                                        <Link href={`/list_objects/create_object/second_step/${itemIdx}`}><Image alt="Редактирование" src={'/Tables/edit.svg'} height={15} width={15}></Image></Link>
+                                        <button onClick={() => deleteJob(item.id)}><Image alt="Редактирование" src={'/Tables/delete.svg'} height={15} width={15}></Image></button>
+                                    </td>
+                                </tr>
+                            ))}
+                        </tbody>
                     </table>
                 </div>
 
                 <button className="self-end bg-red-700 text-white px-6 py-2 rounded-lg">Создать обьект</button>
-            </main>
-        </div>
+            </main >
+        </div >
     )
 }
