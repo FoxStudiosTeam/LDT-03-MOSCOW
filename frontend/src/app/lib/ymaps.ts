4acb0a6e200ef2afcf62dfa2017c673cd9ae3030
makson <maksim.gekk@mail.ru>
2025-09-26 02:28:04 +0300

    map

--- frontend/src/app/lib/ymaps.ts
+++ frontend/src/app/lib/ymaps.ts
@@ -2,28 +2,36 @@ import { YMapReactContainer } from "@yandex/ymaps3-types/imperative/YMapReactCon
 import { ReactifiedModule, Reactify } from "@yandex/ymaps3-types/reactify";
 import React from "react";
 import ReactDOM from "react-dom";
-import { YMap, YMapDefaultSchemeLayer, YMapMarker, YMapDefaultFeaturesLayer } from "ymaps3";
 
 export type YMaps3Modules = ReactifiedModule<Record<string | symbol, unknown> & {
     __implReactifyOverride?: (reactify: Reactify) => object;
-  } & typeof import("r:/prog/LDT-03-MOSCOW/frontend/node_modules/@yandex/ymaps3-types/index") & {
+  } & typeof import("../../../node_modules/@yandex/ymaps3-types/index") & {
       YMapReactContainer: typeof YMapReactContainer;
   }>;
 
-export async function loadYMaps3() {
-  if (typeof window === "undefined") return null;
+
+let _ymaps3Modules: YMaps3Modules | null = null;
+
+export async function loadYMaps3(): Promise<YMaps3Modules> {
+  if (_ymaps3Modules) {
+    console.warn("YMaps3 already loaded");
+    return _ymaps3Modules;
+  };
+  console.warn("YMaps3 loading...");
+
+  if (typeof window === "undefined") throw new Error("YMaps3 can only load in browser");
+
   if (!("ymaps3" in window)) {
-    await new Promise<void>((resolve, reject) => {
-      const check = () => {
-        if ("ymaps3" in window) resolve();
-        else setTimeout(check, 50);
-      };
+    await new Promise<void>((resolve) => {
+      const check = () => ("ymaps3" in window ? resolve() : setTimeout(check, 50));
       check();
     });
   }
+
   await ymaps3.ready;
+
   const ymaps3React = await ymaps3.import("@yandex/ymaps3-reactify");
   const reactify = ymaps3React.reactify.bindTo(React, ReactDOM);
-  const modules : YMaps3Modules = reactify.module(ymaps3);
-  return modules;
+  _ymaps3Modules = reactify.module(ymaps3);
+  return _ymaps3Modules
 }
