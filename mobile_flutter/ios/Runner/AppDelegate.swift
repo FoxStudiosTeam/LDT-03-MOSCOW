24c9e012893c171ed976ee9f393d8b2c81d7bfd7
agniaendie <endienasg2@gmail.com>
2025-10-01 01:14:02 +0500

    fix ocr deps for ios

--- mobile_flutter/ios/Runner/AppDelegate.swift
+++ mobile_flutter/ios/Runner/AppDelegate.swift
@@ -3,22 +3,62 @@ import UIKit
 
 @main
 @objc class AppDelegate: FlutterAppDelegate {
-  override func application(
-    _ application: UIApplication,
-    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
-  ) -> Bool {
-    let controller = window?.rootViewController as! FlutterViewController
-    let channel = FlutterMethodChannel(name: "ocr_channel", binaryMessenger: controller.binaryMessenger)
+    override func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+    ) -> Bool {
+        let controller = window?.rootViewController as! FlutterViewController
+        let channel = FlutterMethodChannel(
+            name: "ocr_channel", binaryMessenger: controller.binaryMessenger)
 
-    channel.setMethodCallHandler { (call, result) in
-        switch call.method {
-        case "getBoxes":
-            result([])
-        default:
-            result(FlutterMethodNotImplemented)
+        channel.setMethodCallHandler { (call, result) in
+            switch call.method {
+            case "getBoxes":
+                guard let args = call.arguments as? [String: Any],
+                    let imageBytes = args["image"] as? FlutterStandardTypedData,
+                    let image = UIImage(data: imageBytes.data)
+                else {
+                    result(
+                        FlutterError(
+                            code: "INVALID_IMAGE", message: "Image data is missing or invalid",
+                            details: nil))
+                    return
+                }
+
+                guard let tesseract = G8Tesseract(language: "rus") else {
+                    result(
+                        FlutterError(
+                            code: "OCR_INIT_FAILED", message: "Tesseract init failed", details: nil)
+                    )
+                    return
+                }
+
+                tesseract.engineMode = .tesseractOnly
+                tesseract.pageSegmentationMode = .auto
+                tesseract.image = image.g8_blackAndWhite()
+                tesseract.recognize()
+
+                var boxes: [[String: Any]] = []
+
+                if let words = tesseract.recognizedBlocks(by: .word) as? [G8RecognizedBlock] {
+                    for word in words {
+                        let rect = word.boundingBox
+                        boxes.append([
+                            "text": word.text ?? "",
+                            "x": Int(rect.origin.x),
+                            "y": Int(rect.origin.y),
+                            "w": Int(rect.width),
+                            "h": Int(rect.height),
+                        ])
+                    }
+                }
+
+                result(boxes)
+            default:
+                result(FlutterMethodNotImplemented)
+            }
         }
+        GeneratedPluginRegistrant.register(with: self)
+        return super.application(application, didFinishLaunchingWithOptions: launchOptions)
     }
-    GeneratedPluginRegistrant.register(with: self)
-    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
-  }
 }
