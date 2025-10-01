ddca6bda4b852edd7d6a1c5696f19b3428aeecf4
agniaendie <endienasg2@gmail.com>
2025-10-01 18:06:44 +0500

    App Delegate fixed.

--- mobile_flutter/ios/Runner/AppDelegate.swift
+++ mobile_flutter/ios/Runner/AppDelegate.swift
@@ -44,67 +44,67 @@ import UIKit
                     let rect = symbol.boundingBox
                     return [
                         "text": symbol.text ?? "",
-                        "confidence": symbol.confidence,
-                        "left": Int(rect.origin.x),
-                        "top": Int(rect.origin.y),
-                        "right": Int(rect.origin.x + rect.width),
-                        "bottom": Int(rect.origin.y + rect.height)
+//                        "confidence": symbol.confidence,
+//                        "left": Int(rect.origin.x),
+//                        "top": Int(rect.origin.y),
+//                        "right": Int(rect.origin.x + rect.width),
+//                        "bottom": Int(rect.origin.y + rect.height)
                     ]
                 }
 
                 func wordToDict(_ word: G8RecognizedBlock) -> [String: Any] {
                     let rect = word.boundingBox
-                    let symbols = (word.symbols ?? []) as! [G8RecognizedBlock]
+//                    let symbols = (word.symbols ?? []) as! [G8RecognizedBlock]
                     return [
                         "text": word.text ?? "",
-                        "confidence": word.confidence,
-                        "left": Int(rect.origin.x),
-                        "top": Int(rect.origin.y),
-                        "right": Int(rect.origin.x + rect.width),
-                        "bottom": Int(rect.origin.y + rect.height),
-                        "symbols": symbols.map(symbolToDict)
+//                        "confidence": word.confidence,
+//                        "left": Int(rect.origin.x),
+//                        "top": Int(rect.origin.y),
+//                        "right": Int(rect.origin.x + rect.width),
+//                        "bottom": Int(rect.origin.y + rect.height),
+//                        "symbols": symbols.map(symbolToDict)
                     ]
                 }
 
                 func lineToDict(_ line: G8RecognizedBlock) -> [String: Any] {
                     let rect = line.boundingBox
-                    let words = (line.words ?? []) as! [G8RecognizedBlock]
+//                    let words = (line.words ?? []) as! [G8RecognizedBlock]
                     return [
                         "text": line.text ?? "",
-                        "confidence": line.confidence,
-                        "left": Int(rect.origin.x),
-                        "top": Int(rect.origin.y),
-                        "right": Int(rect.origin.x + rect.width),
-                        "bottom": Int(rect.origin.y + rect.height),
-                        "words": words.map(wordToDict)
+//                        "confidence": line.confidence,
+//                        "left": Int(rect.origin.x),
+//                        "top": Int(rect.origin.y),
+//                        "right": Int(rect.origin.x + rect.width),
+//                        "bottom": Int(rect.origin.y + rect.height),
+//                        "words": words.map(wordToDict)
                     ]
                 }
 
                 func paragraphToDict(_ para: G8RecognizedBlock) -> [String: Any] {
                     let rect = para.boundingBox
-                    let lines = (para.lines ?? []) as! [G8RecognizedBlock]
+//                    let lines = (para.lines ?? []) as! [G8RecognizedBlock]
                     return [
                         "text": para.text ?? "",
-                        "confidence": para.confidence,
-                        "left": Int(rect.origin.x),
-                        "top": Int(rect.origin.y),
-                        "right": Int(rect.origin.x + rect.width),
-                        "bottom": Int(rect.origin.y + rect.height),
-                        "lines": lines.map(lineToDict)
+//                        "confidence": para.confidence,
+//                        "left": Int(rect.origin.x),
+//                        "top": Int(rect.origin.y),
+//                        "right": Int(rect.origin.x + rect.width),
+//                        "bottom": Int(rect.origin.y + rect.height),
+//                        "lines": lines.map(lineToDict)
                     ]
                 }
 
                 func blockToDict(_ block: G8RecognizedBlock) -> [String: Any] {
                     let rect = block.boundingBox
-                    let paragraphs = (block.paragraphs ?? []) as! [G8RecognizedBlock]
+//                    let paragraphs = (block.paragraphs ?? []) as! [G8RecognizedBlock]
                     return [
                         "text": block.text ?? "",
-                        "confidence": block.confidence,
-                        "left": Int(rect.origin.x),
-                        "top": Int(rect.origin.y),
-                        "right": Int(rect.origin.x + rect.width),
-                        "bottom": Int(rect.origin.y + rect.height),
-                        "paragraphs": paragraphs.map(paragraphToDict)
+//                        "confidence": block.confidence,
+//                        "left": Int(rect.origin.x),
+//                        "top": Int(rect.origin.y),
+//                        "right": Int(rect.origin.x + rect.width),
+//                        "bottom": Int(rect.origin.y + rect.height),
+//                        "paragraphs": paragraphs.map(paragraphToDict)
                     ]
                 }
 
@@ -112,8 +112,8 @@ import UIKit
                 let blocks = tesseract.recognizedBlocks(by: .block) as? [G8RecognizedBlock] ?? []
                 let pageDict: [String: Any] = [
                     "text": tesseract.recognizedText ?? "",
-                    "confidence": tesseract.meanConfidence(),
-                    "blocks": blocks.map(blockToDict)
+//                    "confidence": 0,
+//                    "blocks": blocks.map(blockToDict)
                 ]
 
                 result(pageDict)
